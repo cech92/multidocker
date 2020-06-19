517c17d8d89161072c8867cfd1b49867352b0bf3
Lorenzo Ceccon <lorenzo.ceccon@studenti.unipd.it>
2020-06-19 21:56:09 +0200

    fix frontend

---
+++ frontend/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default() => {
+    return (
+    <div>
+        New page
+        <Link to="/">GO back!</Link>
+    </div>
+    );
+};
